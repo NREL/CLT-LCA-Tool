3cd9b5ef9dad017618dd9714d1c53a896591f4a6
Swara-96 <63014057+Swara-96@users.noreply.github.com>
2021-01-26 15:33:30 -0500

    Taking input from the user for building choice and area

--- Main.py
+++ Main.py
@@ -1,10 +1,5 @@
 ######################################################################################################
-# import all required packages and functions
-from Functions import get_GM_API_Key
-from Functions import lumber_species_properties_processing
-from Functions import calculate_lumber_impact
-from Functions import calculate_distance
-from Functions import calculate_energy_impacts
+##import modules
 import googlemaps
 import gmaps
 import gmaps.datasets
@@ -14,12 +9,25 @@ import pandas as pd
 import xlsxwriter
 import math as math
 import numpy as np
-#import matplotlib
 import matplotlib.pyplot as plt
 
-# from itertools import tee ##this line may not be needed
+##import functions
+from Google_Maps_Functions import get_GM_API_Key
+# from Functions import lumber_species_properties_processing
+# from Functions import calculate_lumber_impact
+# from Functions import calculate_distance
+# from Functions import calculate_energy_impacts
 
 ###import api_key from text file and configure the key
 ###this key will be required to calculate the distance between the clt mill and the building site
 apikey_text = get_GM_API_Key()
 gmaps.configure(api_key=apikey_text)
+
+###Asking the user for the choice of the building
+building_choice = input("Enter your building choice number:")
+building_area_sqft = input("Enter the required building area in square feet (Integer values only):")
+
+
+###
+
+
