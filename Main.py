4736b951405e468e1238d6cf49ead2d9587e93bf
Swara-96 <63014057+Swara-96@users.noreply.github.com>
2021-01-26 11:31:53 -0500

    Importing the GM API Key

--- Main.py
+++ Main.py
@@ -18,3 +18,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # from itertools import tee ##this line may not be needed
+
+###import api_key from text file and configure the key
+###this key will be required to calculate the distance between the clt mill and the building site
+apikey_text = get_GM_API_Key()
+gmaps.configure(api_key=apikey_text)
