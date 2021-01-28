0442914514a7fc8e26f92b76c42b9da3864e5383
Swara-96 <63014057+Swara-96@users.noreply.github.com>
2021-01-28 15:16:08 -0500

    Adding code to calculate amount of lumber req to produce 1 m3 clt fr each species

--- Main.py
+++ Main.py
@@ -44,7 +44,7 @@ multiplier = building_area_sqft_required/building_area_sqft_given_building
 
 building_inventory_table_CLT_building["Normalized_Quantity_LCA"] = building_inventory_table_CLT_building["Quantity_LCA"] * multiplier
 
-CLT_required = building_inventory_table_CLT_building.loc[building_inventory_table_CLT_building.Material == "CLT", 'Quantity'].sum()
+CLT_required = building_inventory_table_CLT_building.loc[building_inventory_table_CLT_building.Material == "CLT", 'Normalized_Quantity_LCA'].sum()
 
 ###read data for concrete building
 
@@ -52,3 +52,36 @@ building_info_trad_building = pd.read_excel("C:/Users/SATNOORK/Documents/GitHub/
 
 building_inventory_table_trad_building = pd.read_excel("C:/Users/SATNOORK/Documents/GitHub/CLT-LCA-Tool/Building_Configurations.xlsx", sheet_name='Lever_Concrete', skiprows=9,nrows=40)
 
+###create results dataframes ###
+
+energy_impacts_CLT = pd.DataFrame(columns=["Phase", "Renewable, wind, solar, geothe", "Renewable, biomass", "Non renewable, fossil", "Non-renewable, nuclear", "Renewable, water", "Non-renewable, biomass"])
+phases_1 = ["Lumber Production", "Transport to CLT Mill", "CLT Manufacturing", "Transport to building site"]
+energy_impacts_CLT["Phase"] = phases_1
+
+energy_impacts_CLT_building = pd.DataFrame(columns=["Phase", "Non renewable, fossil","Non-renewable, nuclear","Non-renewable, biomass", "Renewable, wind, solar, geothe", "Renewable, biomass",   "Renewable, water" ])
+phases_2 = ["A1", "A2", "A3", "A4", "A5"]
+energy_impacts_CLT_building["Phase"] = phases_2
+
+###############################################################################################################
+###module to calculate the impacts of lumber production###
+
+###read timber data
+timber_species_properties = pd.read_excel("C:/Users/SATNOORK/Documents/GitHub/CLT-LCA-Tool/CLT_Timber_types.xlsx",sheet_name="ALSC PS 20 Lumber Species",skiprows=1)
+
+###waste_percentage = input("Enter the % of wood (by weight) lost in CLT manufacturing")
+waste_percentage = 16
+
+timber_species_properties["Final Product Weight (odkg/m3 CLT)_"] = timber_species_properties["Specific Gravity : 12%"]*1000
+timber_species_properties["Co Products Weight (odkg/m3 CLT)_"] = timber_species_properties["Final Product Weight (odkg/m3 CLT)"]*waste_percentage/(100-waste_percentage)
+timber_species_properties["Total Wood Weight (odkg/m3 CLT)_"] = timber_species_properties["Final Product Weight (odkg/m3 CLT)"] + timber_species_properties["Co Products Weight (odkg/m3 CLT)"]
+timber_species_properties["Total 12% Wood Vol Req (m3/m3 CLT)_"] = timber_species_properties["Total Wood Weight (odkg/m3 CLT)"]/timber_species_properties["Specific Gravity : 12%"]/1000
+timber_species_properties["Total Green Wood Vol Req (m3/m3 CLT)_"] = timber_species_properties["Total Wood Weight (odkg/m3 CLT)"]/timber_species_properties["Specific Gravity : Green"]/1000
+
+
+#for now, this file has been directly read in through Excel. In further iteration, make this a variable by using the waste_percentage.
+
+#lumber_species_properties_processed = lumber_species_properties_processing(waste_percentage, Lumber_species_properties)
+
+###add values for lumber transport and processing
+
+#lumber_required = CLT_required * lumber_species_properties_processed.loc[lumber_species_properties_processed["ALSC PS 20 Commercial Species"] == timber_type, 'Total 12% Wood Vol Req (m3/m3 CLT)'].item()
